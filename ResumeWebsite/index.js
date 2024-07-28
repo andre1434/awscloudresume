fdc020bf46a241257054f450519cad8df782b26a
andre1434 <andresoller22@gmail.com>
2024-07-28 14:20:57 +0800

    update index.js

--- ResumeWebsite/index.js
+++ ResumeWebsite/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter(){
 	let response = await fetch("https://brn5mbllmnthhweoexudmlmzfy0pryts.lambda-url.us-west-2.on.aws/");
 	let data = await response.json();
-	counter.innerHTML = ` Website Views: ${views}`;
+	counter.innerHTML = ` Website Views: ${data.views}`;
 }
 
 updateCounter();
